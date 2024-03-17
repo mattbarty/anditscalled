2cce945920d273bc68c2a0eeaa0421d019d93f84
Matt Barty <Matthew.barty@outlook.com>
2024-03-17 07:58:56 +0000

    Update min-height of main container

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Heart } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24 overflow-clip mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <main className="flex min-h-[95vh] md:min-h-screen flex-col items-center justify-between p-6 md:p-24 overflow-clip mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg w-full">
         <DomainGenerator />
       </div>
